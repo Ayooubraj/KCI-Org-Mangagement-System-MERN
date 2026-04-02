22c8e1587f4429be601c9d007688d9fddc580902
Ayooubraj <ayooubrg@gmail.com>
2026-04-02 10:37:30 +0545

    Admin Homepage fixed

--- frontend/src/pages/admin/AdminHomePage.js
+++ frontend/src/pages/admin/AdminHomePage.js
@@ -1,4 +1,4 @@
-import { Container, Grid, Paper } from '@mui/material'
+import { Container, Grid, Paper } from '@mui/material';
 import SeeNotice from '../../components/SeeNotice';
 import Students from "../../assets/img1.png";
 import Classes from "../../assets/img2.png";
@@ -11,77 +11,69 @@ import { useEffect } from 'react';
 import { getAllSclasses } from '../../redux/sclassRelated/sclassHandle';
 import { getAllStudents } from '../../redux/studentRelated/studentHandle';
 import { getAllTeachers } from '../../redux/teacherRelated/teacherHandle';
+import { getAllDonors } from '../../redux/donorRelated/donorHandle'; // ✅ new
 
 const AdminHomePage = () => {
-    const dispatch = useDispatch();
-    const { studentsList } = useSelector((state) => state.student);
-    const { sclassesList } = useSelector((state) => state.sclass);
-    const { teachersList } = useSelector((state) => state.teacher);
+  const dispatch = useDispatch();
+  const { studentsList } = useSelector((state) => state.student);
+  const { sclassesList } = useSelector((state) => state.sclass);
+  const { teachersList } = useSelector((state) => state.teacher);
+  const { donorsList } = useSelector((state) => state.donor); // ✅ new
 
-    const { currentUser } = useSelector(state => state.user)
+  const { currentUser } = useSelector(state => state.user);
+  const adminID = currentUser._id;
 
-    const adminID = currentUser._id
+  useEffect(() => {
+    dispatch(getAllStudents(adminID));
+    dispatch(getAllDonors(adminID));   // ✅ new
+    dispatch(getAllSclasses(adminID, "Sclass"));
+    dispatch(getAllTeachers(adminID));
+  }, [adminID, dispatch]);
 
-    useEffect(() => {
-        dispatch(getAllStudents(adminID));
-        dispatch(getAllSclasses(adminID, "Sclass"));
-        dispatch(getAllTeachers(adminID));
-    }, [adminID, dispatch]);
+  const numberOfStudents = studentsList?.length || 0;
+  const numberOfDonors = donorsList?.length || 0;   // ✅ new
+  const numberOfTeachers = teachersList?.length || 0;
 
-    const numberOfStudents = studentsList && studentsList.length;
-    const numberOfClasses = sclassesList && sclassesList.length;
-    const numberOfTeachers = teachersList && teachersList.length;
-
-    return (
-        <>
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <StyledPaper>
-                            <img src={Students} alt="Students" />
-                            <Title>
-                                Total Sponsored Students
-                            </Title>
-                            <Data start={0} end={numberOfStudents} duration={2.5} />
-                        </StyledPaper>
-                    </Grid>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <StyledPaper>
-                            <img src={Classes} alt="Classes" />
-                            <Title>
-                                Total Donors
-                            </Title>
-                            <Data start={0} end={numberOfClasses} duration={5} />
-                        </StyledPaper>
-                    </Grid>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <StyledPaper>
-                            <img src={Teachers} alt="Teachers" />
-                            <Title>
-                                Total Staffs
-                            </Title>
-                            <Data start={0} end={numberOfTeachers} duration={2.5} />
-                        </StyledPaper>
-                    </Grid>
-                    <Grid item xs={12} md={3} lg={3}>
-                        <StyledPaper>
-                            <img src={Fees} alt="Fees" />
-                            <Title>
-                                Fees Collection
-                            </Title>
-                            <Data start={0} end={23000} duration={2.5} prefix="$" />                        </StyledPaper>
-                    </Grid>
-                    <Grid item xs={12} md={12} lg={12}>
-                        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                            <SeeNotice />
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Container>
-        </>
-    );
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={3} lg={3}>
+          <StyledPaper>
+            <img src={Students} alt="Students" />
+            <Title>Total Sponsored Students</Title>
+            <Data start={0} end={numberOfStudents} duration={2.5} />
+          </StyledPaper>
+        </Grid>
+        <Grid item xs={12} md={3} lg={3}>
+          <StyledPaper>
+            <img src={Classes} alt="Donors" />
+            <Title>Total Donors</Title>
+            <Data start={0} end={numberOfDonors} duration={2.5} />
+          </StyledPaper>
+        </Grid>
+        <Grid item xs={12} md={3} lg={3}>
+          <StyledPaper>
+            <img src={Teachers} alt="Teachers" />
+            <Title>Total Staffs</Title>
+            <Data start={0} end={numberOfTeachers} duration={2.5} />
+          </StyledPaper>
+        </Grid>
+        <Grid item xs={12} md={3} lg={3}>
+          <StyledPaper>
+            <img src={Fees} alt="Fees" />
+            <Title>Fees Collection</Title>
+            <Data start={0} end={23000} duration={2.5} prefix="$" />
+          </StyledPaper>
+        </Grid>
+        <Grid item xs={12} md={12} lg={12}>
+          <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+            <SeeNotice />
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 };
-
 
 const StyledPaper = styled(Paper)`
   padding: 16px;
@@ -102,4 +94,4 @@ const Data = styled(CountUp)`
   color: green;
 `;
 
-export default AdminHomePage
+export default AdminHomePage;
