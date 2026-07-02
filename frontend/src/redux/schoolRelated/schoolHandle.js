5943d03079452e4adefa7f6ffa16fa3ff9dde5e7
Ayooub <ayooubrg@gmail.com>
2026-07-02 11:08:09 +0545

    school handle updated

--- frontend/src/redux/schoolRelated/schoolHandle.js
+++ frontend/src/redux/schoolRelated/schoolHandle.js
@@ -1,8 +1,12 @@
+// schoolHandle.js
+
 import axios from "axios";
+
+const API_URL = "http://localhost:5000";
 
 export const getAllSchools = () => async (dispatch) => {
   try {
-    const res = await axios.get("/Schools");
+    const res = await axios.get(`${API_URL}/Schools`);
     dispatch({ type: "SET_SCHOOLS", payload: res.data });
   } catch (err) {
     console.error("Error fetching schools", err);
